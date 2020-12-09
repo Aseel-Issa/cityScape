d197a571d40165015ce1725cc7671b0308b8e9ee
Ido Erez <idodoerez@gmail.com>
2020-12-09 14:39:38 +0200

    Finished api calls

--- Server.js
+++ Server.js
@@ -1,16 +1,18 @@
 const express = require('express')
-// const api = require('./server/routes/api')
+const api = require('./server/routes/api')
 const path = require('path')
 const app = express()
+require('dotenv').config()
+const {PORT} = process.env
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use('/', api)
+app.use('/', api)
 
-const port = 3000
-app.listen(port, function () {
-    console.log(`Running server on port ${port}`)
+
+app.listen(PORT, function () {
+    console.log(`Running server on port ${PORT}`)
 })
