269e5591a3358c55b2a142b99c825f3d0491b37b
aseel <aseelissa91@hotmail.com>
2020-12-10 00:57:12 +0200

    integrating the client side with server side, but not tested yet

--- dist/AppManager.js
+++ dist/AppManager.js
@@ -1,19 +1,57 @@
 class AppManager {
     constructor() {
-            }
+    }
 
+    /**
+     * 
+     * @param {*} username 
+     * @param {*} password 
+     * @returns a user object
+     */
     async signUp(username, password) {
-        const user = new User(await $.post('/myCityScape', username, password))
+        let user = {};
+        try {
+            const result = await $.ajax({
+                url: '/mycityscape/createUser',
+                type: 'POST',
+                data: {
+                    userName: username,
+                    userPassword: password
+                }
+            });
+            user.username = username
+            user.password = password
+            user.id = result
+            user.isLoggedIn = true
+        } catch (error) {
+            console.error(error);
+        }
         return user
     }
 
     async logIn(username, password) {
-        const user = new User (await $.get('/myCityScape', username, password)) 
-        user.isLoggedIn = true 
+
+        let user = {};
+        try {
+            const result = await $.ajax({
+                url: '/mycityscape/user/trips',
+                type: 'GET',
+                data: {
+                    userName: username,
+                    userPassword: password
+                }
+            });
+            user.username = username
+            user.password = password
+            user.id = result
+            user.isLoggedIn = true
+        } catch (error) {
+            console.error(error);
+        }
         return user
     }
 
     async logOut(user) {
-       user.isLoggedIn = false         
+        user.isLoggedIn = false
     }
 }
